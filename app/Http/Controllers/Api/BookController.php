7dcaf2c6e1d675cddec116315ed733a6a1e84087
Helios360 <elias.treguier@gmail.com>
2026-02-09 13:04:30 +0100

    act 2

---
+++ app/Http/Controllers/Api/BookController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use App\Http\Resources\BookResource;
+use App\Http\Controllers\Controller;
+use App\Models\Book;
+
+class BookController extends Controller
+{
+    public function index(){
+        return BookResource::collection(Book::query()->latest()->paginate(10));
+    }
+    public function store(Request $request){
+        $validated = $request->validate([
+            'title' => ['required', 'string', 'min:3', 'max:255'],
+            'author' => ['required', 'string', 'min:3', 'max:100'],
+            'summary' => ['required', 'string', 'min:10', 'max:500'],
+            'isbn' => ['required', 'string', 'size:13', 'unique:books,isbn'],
+        ]);
+        $book = Book::create($validated);
+        return (new BookRessource($book))->response()->setStatusCode(201);
+    }
+    public function show(Book $book){return new BookResource($book);}
+    public function update(Request $request, Book $book){
+        $validated = $request->validate([
+            'title' => ['required', 'string', 'min:3', 'max:255'],
+            'author' => ['required', 'string', 'min:3', 'max:100'],
+            'summary' => ['required', 'string', 'min:10', 'max:500'],
+            'isbn' => ['required', 'string', 'size:13', Rule::unique('books', 'isbn')->ignore($book->id)],
+        ]);
+        $book->update($validated);
+        return new BookRessource($book);
+    }
+    public function destroy(Book $book){
+        $book->delete();
+        return response()->json(null, 204);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\BookController;
+
+Route::prefix('v1')->group(function (){
+    Route::apiResource('books', BookController::class);
+});
 
 Route::get('/ping', function () {
     return response()->json([
